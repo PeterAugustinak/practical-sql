4a2e27b1cb9ac1f34884cdc226136031d2c1e73e
PeterAugustinak <peter.augustinak@gmail.com>
2022-07-10 12:40:34 +0200

    Chapter7 - customized columns in joins, set operators (UNION, INTERSECT, EXCEPT)

--- src/chapter7_joins.sql
+++ src/chapter7_joins.sql
@@ -119,3 +119,112 @@ FROM district_2020 CROSS JOIN district_2035
 ORDER BY district_2020.id, district_2035.id;
 
 -- using NULL to find rows with missing values
+SELECT *
+FROM district_2020 LEFT JOIN district_2035
+USING (id)
+WHERE district_2035.id IS NULL;
+-- WHERE district_2035 IS NOT NULL;
+
+-- selecting specific columns in a JOIN
+SELECT id -- possible as the column id is present in both tables
+FROM district_2020 LEFT JOIN district_2035
+USING (id);
+
+SELECT district_2020.id,
+       district_2020.school_2020,
+       district_2035.school_2035
+FROM district_2020 LEFT JOIN district_2035
+USING (id)
+ORDER BY district_2020.id;
+
+-- simplifying JOIN syntax with table aliases
+SELECT d20.id,
+       d20.school_2020,
+       d35.school_2035
+FROM district_2020 AS d20 LEFT JOIN district_2035 AS d35
+ON d20.id = d35.id
+ORDER BY d20.id;
+
+-- joining multiple tables
+CREATE TABLE district_2020_enrollment (
+    id integer,
+    enrollment integer
+);
+
+CREATE TABLE district_2020_grades (
+    id integer,
+    grades varchar(10)
+);
+
+INSERT INTO district_2020_enrollment
+VALUES
+    (1, 360),
+    (2, 1001),
+    (5, 450),
+    (6, 927);
+
+INSERT INTO district_2020_grades
+VALUES
+    (1, 'K-3'),
+    (2, '9-12'),
+    (5, '6-8'),
+    (6, '9-12');
+
+SELECT * FROM district_2020_enrollment;
+SELECT * FROM district_2020_grades;
+
+SELECT d20.id,
+       d20.school_2020,
+       en.enrollment,
+       gr.grades
+FROM district_2020 AS d20
+JOIN district_2020_enrollment AS en
+    ON d20.id = en.id
+JOIN district_2020_grades AS gr
+    ON d20.id = gr.id
+ORDER BY d20.id;
+
+-- combining query result with set operators
+/*
+ UNION - append results from two queries, removes duplicates, distinct rows
+ UNION ALL - all results including duplicates
+ INTERSECT - returns only rows exist in the results of both, removing duplicates
+ EXCEPT - returns result exist in first query but not in the second query, duplicates removed
+ */
+
+-- UNION
+SELECT * FROM district_2020
+UNION
+SELECT * FROM district_2035
+ORDER BY id;
+
+-- UNION ALL
+SELECT * FROM district_2020
+UNION ALL
+SELECT * FROM district_2035
+ORDER BY id;
+
+-- customizing result by adding "default" column to inform about table
+SELECT '2020' AS year,
+       school_2020 AS school
+FROM district_2020
+UNION ALL
+SELECT '2035' AS year,
+       school_2035
+FROM district_2035
+ORDER BY year, school;
+
+-- INTERSECT
+SELECT * FROM district_2020
+INTERSECT
+SELECT * FROM district_2035
+ORDER BY id;
+
+-- EXCEPT
+SELECT * FROM district_2020
+EXCEPT
+SELECT * FROM district_2035
+ORDER BY id;
+
+-- performing math in JOINed table columns
+
