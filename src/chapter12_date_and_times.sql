9a2463fa29dcab53e956f1166c92e39ba4177000
PeterAugustinak <peter.augustinak@gmail.com>
2022-07-20 13:20:13 +0200

    Chapter12 - adding nyc_taxi table

--- src/chapter12_date_and_times.sql
+++ src/chapter12_date_and_times.sql
@@ -45,7 +45,7 @@ SELECT * FROM current_time_example;
 
 -- working with timezones
 SHOW timezone;
-SELECT current_setting('timezone')
+SELECT current_setting('timezone');
 
 SELECT make_timestamptz(2022, 2, 22, 18, 4, 30.3, current_setting('timezone'));
 
@@ -78,4 +78,69 @@ FROM time_zone_test;
 -- calculations with date and times
 SELECT '1929-09-30'::date - '1929-09-27'::date;
 SELECT '1929-09-30'::date + '5 years'::interval;
+
+-- NYC taxi data
+CREATE TABLE nyc_yellow_taxi_trips (
+    trip_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    vendor_id text NOT NULL,
+    tpep_pickup_datetime timestamptz NOT NULL,
+    tpep_dropoff_datetime timestamptz NOT NULL,
+    passenger_count integer NOT NULL,
+    trip_distance numeric(8,2) NOT NULL,
+    pickup_longitude numeric(18,15) NOT NULL,
+    pickup_latitude numeric(18,15) NOT NULL,
+    rate_code_id text NOT NULL,
+    store_and_fwd_flag text NOT NULL,
+    dropoff_longitude numeric(18,15) NOT NULL,
+    dropoff_latitude numeric(18,15) NOT NULL,
+    payment_type text NOT NULL,
+    fare_amount numeric(9,2) NOT NULL,
+    extra numeric(9,2) NOT NULL,
+    mta_tax numeric(5,2) NOT NULL,
+    tip_amount numeric(9,2) NOT NULL,
+    tolls_amount numeric(9,2) NOT NULL,
+    improvement_surcharge numeric(9,2) NOT NULL,
+    total_amount numeric(9,2) NOT NULL
+);
+
+COPY nyc_yellow_taxi_trips (
+    vendor_id,
+    tpep_pickup_datetime,
+    tpep_dropoff_datetime,
+    passenger_count,
+    trip_distance,
+    pickup_longitude,
+    pickup_latitude,
+    rate_code_id,
+    store_and_fwd_flag,
+    dropoff_longitude,
+    dropoff_latitude,
+    payment_type,
+    fare_amount,
+    extra,
+    mta_tax,
+    tip_amount,
+    tolls_amount,
+    improvement_surcharge,
+    total_amount
+   )
+FROM '/var/lib/postgresql/nyc_yellow_taxi_trips.csv'
+WITH (FORMAT CSV, HEADER);
+
+CREATE INDEX tpep_pickup_idx
+ON nyc_yellow_taxi_trips (tpep_pickup_datetime);
+
+-- count the trip record
+SELECT count(*) FROM new_york_addresses;
+
+-- counting taxi trips by hour
+SET TIME ZONE 'US/Eastern';
+SHOW timezone ;
+
+SELECT
+    date_part('hour', tpep_pickup_datetime) AS trip_hour,
+    count(*)
+FROM nyc_yellow_taxi_trips
+GROUP BY trip_hour
+ORDER BY trip_hour;
 
