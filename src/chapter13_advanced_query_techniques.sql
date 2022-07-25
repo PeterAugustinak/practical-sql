25e569dd0f3fbf921a517a23330fddb63b504e31
PeterAugustinak <peter.augustinak@gmail.com>
2022-07-25 19:33:06 +0200

    Chapter13 - WITH reference table

--- src/chapter13_advanced_query_techniques.sql
+++ src/chapter13_advanced_query_techniques.sql
@@ -157,3 +157,50 @@ ORDER BY t.id;
  works as a for loop - for every row returned from SELECT, the subquery is
  executed
  */
+
+
+-- Common table expressions
+WITH large_counties (county_name, state_name, pop_est_2019)
+AS (SELECT county_name, state_name, pop_est_2019
+    FROM us_counties_pop_est_2019
+    WHERE pop_est_2019 >= 100000)
+SELECT state_name, count(*) AS counties_count
+FROM large_counties
+GROUP BY state_name
+ORDER BY count(*) DESC;
+
+-- WITH + JOIN
+WITH
+    counties (st, pop_est_2018)
+    AS (SELECT state_name, sum(pop_est_2018)
+        FROM us_counties_pop_est_2019
+        GROUP BY state_name),
+    establishments (st, establishment_count)
+    AS (SELECT st, sum(establishments) AS establishments_count
+        FROM cbp_naics_72_establishments
+        GROUP BY st)
+
+SELECT counties.st,
+       pop_est_2018,
+       establishment_count,
+       round((establishments.establishment_count /
+              counties.pop_est_2018::numeric(10, 1)) * 1000, 1 )
+AS estabs_per_tousand
+FROM counties JOIN establishments
+ON counties.st = establishments.st
+ORDER BY estabs_per_tousand DESC;
+
+-- we do not need to define us_median_pop twice, WITH table can be referenced
+WITH us_median
+AS (SELECT percentile_cont(.5)
+    WITHIN GROUP (ORDER BY pop_est_2019) AS us_median_pop
+    FROM us_counties_pop_est_2019)
+
+SELECT county_name,
+       state_name AS st,
+       pop_est_2019,
+       us_median_pop,
+       pop_est_2019 - us_median_pop AS diff_from_median
+FROM us_counties_pop_est_2019
+CROSS JOIN us_median
+WHERE (pop_est_2019 - us_median_pop) BETWEEN -1000 AND 1000;
